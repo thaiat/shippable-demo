3da5e4a4e478269e5f1c8b48828e09e2a1119620
thaiat <thaiat@ipelia.fr>
2014-06-24 02:26:32 +0300

    Add dummy function to verify code coverage

--- scripts/app.js
+++ scripts/app.js
@@ -2,5 +2,8 @@ var app = angular.module('app', []);
 
 app.controller('helloWorldController', function ($scope) {
 	$scope.message = 'Hello World';
+	$scope.dummyFn = function () {
+		console.log($scope.message);
+	};
 
 });
