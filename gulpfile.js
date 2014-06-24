3fc6584d09efb7ec415a5c7db408a7fae7778d7f
thaiat <thaiat@ipelia.fr>
2014-06-25 01:27:57 +0300

    Print all env variables in gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,5 @@ var gulp = require('gulp');
 gulp.task('default', function () {
 	console.log('COMPUTERNAME : ' + process.env.COMPUTERNAME);
 	console.log('BRANCH : ' + process.env.BRANCH);
+	console.log('BRANCH : ' + process.env);
 });
