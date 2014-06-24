f62e7fef9ea460bb56853c854ad0ef8ed207dfa2
thaiat <thaiat@ipelia.fr>
2014-06-24 14:52:43 +0300

    Fix app.js and install express

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var port = process.env.PORT || 5000;
 var app = express();
-
+app.use('/', express.static(__dirname + '/'));
 app.get('/', function (request, response) {
 	response.sendfile(__dirname + '/index.html');
-}).configure(function () {
-	app.use('/', express.static(__dirname + '/'));
 }).listen(port);
