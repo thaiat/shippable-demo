75e2a162bada86934ed721665671469b41658da0
thaiat <thaiat@ipelia.fr>
2014-06-25 01:38:57 +0300

    Update bower

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,6 @@ var gulp = require('gulp');
 gulp.task('default', function () {
 	console.log('COMPUTERNAME : ' + process.env.COMPUTERNAME);
 	console.log('BRANCH : ' + process.env.BRANCH);
+	console.log('AND ALL NOW ----');
 	console.log('ALL : ' + JSON.stringify(process.env));
 });
