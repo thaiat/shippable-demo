b181e24925a68c6db7ab460990dd5651a58199e5
thaiat <thaiat@ipelia.fr>
2014-06-25 01:43:27 +0300

    Fix print env variables

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,8 @@ gulp.task('default', function () {
 	console.log('COMPUTERNAME : ' + process.env.COMPUTERNAME);
 	console.log('BRANCH : ' + process.env.BRANCH);
 	console.log('AND ALL NOW ----');
-	console.log('ALL : ' + JSON.stringify(process.env));
+	for (var key in process.env) {
+		console.log(key);
+	}
+
 });
